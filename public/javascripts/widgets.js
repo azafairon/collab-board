3b59e167e2d9303bd7c33c398724f58bc25a226a
noriaf <azafairon@gmail.com>
2016-01-27 23:10:52 +0000

    some testing

--- public/javascripts/widgets.js
+++ public/javascripts/widgets.js
@@ -2,10 +2,11 @@ $(function() {
 var socket = io();
 
 socket.on('update_position',function(data){
-	console.log('recibido ' + data);
 	var x = data.x;
 	var y = data.y;
-	$(".dragme").css({
+	var id = data.id;
+
+	$('#'+ id).css({
 		left: x + "px",
 		top: y + "px"
 	});
@@ -13,10 +14,12 @@ socket.on('update_position',function(data){
 });
 
 socket.on('update_size',function(data){
-	console.log('recibido ' + data);
+
 	var w = data.w;
 	var h = data.h;
-	$(".dragme").css({
+	var id = data.id;
+
+	$('#'+ id).css({
 		height: h + "px",
 		width: w + "px"
 	});
@@ -24,21 +27,14 @@ socket.on('update_size',function(data){
 });
 
 $( ".dragme" ).resizable({
-	containment : "parent",
-	stop: function(e, ui) {
-		var width = ui.size.width;
-		var height = ui.size.height;
-		var hereDrag = this;
-
-		if($(hereDrag).find('textarea').length > 0){
-			$(hereDrag).find('textarea').css('width', width - 10);
-			$(hereDrag).find('textarea').css('height', height - 10);
-		}
-	},
 	resize: function (event, ui) {
-		var h = $(this).height();
-		var w = $(this).width();
+
+		var h  = $(this).height();
+		var w  = $(this).width();
+		var id = $(this).attr('id');
+
 		socket.emit('size', {
+				id: id,
 				h: h,
 				w: w 
 			});
@@ -52,8 +48,12 @@ $( ".dragme" ).draggable({
 	scroll      : false,
 	cancel: "",
 	drag: function (event, ui) {
+
 		var coord = $(this).position();
+		var id    = $(this).attr('id');
+
 		socket.emit('position', {
+				id: id,
 				x: coord.left,
 				y: coord.top
 			});
